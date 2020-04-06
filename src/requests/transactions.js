1ecd76b37561bdb3d2e0a04eb1d0ed74f62535ae
devskope <ope.oguntoye@protonmail.com>
2020-04-06 02:06:40 +0100

    add transaction creation request logic [#5]

---
+++ src/requests/transactions.js
@@ -0,0 +1,26 @@
+import bent from 'bent';
+
+const apiPost = bent(process.env.REACT_APP_API_URL, 'POST', 'json');
+
+export const createTransaction = async payload => {
+  const response = { data: null, error: null };
+
+  try {
+    const { data } = await apiPost('/initTx', payload);
+    response.data = data;
+  } catch (error) {
+    response.error = await handleApiError(error);
+  }
+
+  return response;
+};
+
+const handleApiError = async error => {
+  if (error.message.includes('Incorrect statusCode:')) {
+    return JSON.parse(new TextDecoder().decode(await error.responseBody)).error;
+  }
+
+  return {
+    message: 'An error occured. Please check your connection and try again'
+  };
+};
