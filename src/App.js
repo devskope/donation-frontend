638346c899b7618eb05915d8d7d3a594d8ce031a
devskope <ope.oguntoye@protonmail.com>
2020-04-07 01:48:14 +0100

    add nav icon to donation form header [#9]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Header, Image } from 'semantic-ui-react';
+import { BrowserRouter, Link, useHistory, useLocation } from 'react-router-dom';
+import { Header, Image, Icon } from 'semantic-ui-react';
 
 import { RenderRoutes, routes } from './routes';
 import logo from './assets/logo.svg';
+
+const Back = () => {
+  const location = useLocation();
+  const { goBack } = useHistory();
+  return (
+    location.pathname === '/donate' && <Icon
+      name='long arrow alternate left'
+      onClick={goBack}
+      size='big' />
+  );
+};
 
 const App = () => {
   return (
     <div className='app'>
       <BrowserRouter>
         <header className='header'>
+          <Back />
           <Header as={Link} to='/'>
             <Image className='logo' centered src={logo} alt='logo' />
             <br />
